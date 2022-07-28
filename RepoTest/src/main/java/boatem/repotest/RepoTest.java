acbaea34b49e2f338a9fa44ef8a8f2c09d6e45da
CliftonBartholomew <cliftonbartholomew@gmail.com>
2022-07-28 12:37:43 +0200

    Clifton added his name [#1]

--- RepoTest/src/main/java/boatem/repotest/RepoTest.java
+++ RepoTest/src/main/java/boatem/repotest/RepoTest.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
-
 package boatem.repotest;
 
 /**
@@ -11,7 +10,8 @@ package boatem.repotest;
  */
 public class RepoTest {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Clifton");
+	}
 }

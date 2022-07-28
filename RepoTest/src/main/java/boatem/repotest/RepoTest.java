d74b10831322a0890369eb7d3b79879d67540987
Jcj <Jcj@ZAKZRHB-CLLB12.ballito.local>
2022-07-28 12:27:34 +0200

    Hello World

---
+++ RepoTest/src/main/java/boatem/repotest/RepoTest.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package boatem.repotest;
+
+/**
+ *
+ * @author Jcj
+ */
+public class RepoTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
